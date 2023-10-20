817ebacf3c52273d4b5ae0d0add7ba5ea6a790e8
Melawatii <mellawati16016@gmail.com>
2023-10-20 14:14:52 +0700

    be create

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Students;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Students::all();
+        return response()->json($students);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $students = new Students();
+        $students->nis = $request->input('nis');
+        $students->name = $request->input('name');
+        $students->class = $request->input('class');
+        $students->save();
+        return response()->json($students);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $students = Students::find($id);
+        $students->nis = $request->input('nis');
+        $students->name = $request->input('name');
+        $students->class = $request->input('class');
+        $students->save();
+        return response()->json($students);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $students = Students::find($id);
+        $students->delete();
+        return response()->json(['message' => 'Students deleted']);
+    }
+}
